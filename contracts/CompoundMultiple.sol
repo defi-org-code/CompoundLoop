aeea6c58bf92820d301853d9a8d091d3de016860
Or <or.dd100@gmail.com>
2021-02-02 23:01:23 +0200

    Imports were updated.

--- contracts/CompoundMultiple.sol
+++ contracts/CompoundMultiple.sol
@@ -2,12 +2,9 @@
 
 pragma solidity ^0.7.3;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "./Exponential.sol";
-
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/access/Ownable.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/SafeERC20.sol";
 
 interface IERC20Detailed {
     function decimals() external view returns (uint8);
