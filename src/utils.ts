2b380683c8de87021d8f0698f3d36b7d48231074
DanielZlotin <zlotindaniel@gmail.com>
2021-02-04 18:28:04 +0200

    productionize tests WIP

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,71 @@
+import { IERC20 } from "../typechain-hardhat/IERC20";
+import { CERC20 } from "../typechain-hardhat/CERC20";
+
+export function bn(n: number | string) {
+  return hre().web3.utils.toBN(n);
+}
+
+export function eth(n: number | string) {
+  return bn(n).muln(1e18);
+}
+
+export function usd(n: number | string) {
+  return bn(n).muln(1e6);
+}
+
 export function hre() {
   return require("hardhat");
 }
 
 export function contract<T>(abi: any, address?: string) {
   return (new (hre().web3.eth.Contract)(abi, address) as any) as T;
+}
+
+export function erc20(address?: string) {
+  const abi = hre().artifacts.readArtifactSync("IERC20").abi;
+  return contract<IERC20>(abi, address);
+}
+
+export function cerc20(address?: string) {
+  const abi = hre().artifacts.readArtifactSync("CERC20").abi;
+  return contract<CERC20>(abi, address);
+}
+
+export async function compiledContract<T>(name: string, owner: string, ...args: any[]) {
+  const artifact = hre().artifacts.readArtifactSync(name);
+  const theContract = new (hre().web3.eth.Contract)(artifact.abi, null, {
+    data: artifact.bytecode,
+    from: owner,
+  });
+  const tx = theContract.deploy({ arguments: [...args] });
+  const result = await tx.send({ from: owner });
+  return result as T; // TODO add .address to prototype
+}
+
+export async function impersonate(address: string) {
+  await hre().network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [address],
+  });
+}
+
+export async function resetNetworkFork(blockNumber?: number) {
+  await hre().network.provider.request({
+    method: "hardhat_reset",
+    params: [
+      {
+        forking: {
+          blockNumber,
+          jsonRpcUrl: hre().network.config.forking.jsonRpcUrl,
+        },
+      },
+    ],
+  });
+}
+
+export async function evmIncreaseTime(seconds: number) {
+  await hre().network.provider.request({
+    method: "evm_increaseTime",
+    params: [seconds],
+  });
 }
