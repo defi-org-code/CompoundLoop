70665ab15aad76e148ff4df00b6d8d627e5d6e9a
DanielZlotin <zlotindaniel@gmail.com>
2021-02-08 12:58:45 +0200

    print claimable comp

--- src/balance.ts
+++ src/balance.ts
@@ -1,11 +1,59 @@
-import Web3 from "web3";
-// import { CompoundLensAbi } from "../typechain/CompoundLensAbi";
-import { contract } from "./utils";
-import { compoundLensAddress, CUSDCAddress } from "./consts";
+import { bn, contract, fmt, hre, keepTrying } from "./utils";
+import { CompoundLoop } from "../typechain-hardhat/CompoundLoop";
+import { compoundLensAddress, compTokenAddress, CONTRACT_ADDRESS, OWNER, unitrollerAddress } from "./consts";
+import { CompoundLensAbi } from "../typechain/CompoundLensAbi";
 
-// const lensAbi = require("../abi/CompoundLensAbi.json");
+const abi = require("../artifacts/contracts/CompoundLoop.sol/CompoundLoop.json").abi;
 
-export async function getBalance(web3: Web3, owner: string) {
-  // const lens = contract<CompoundLensAbi>(web3, lensAbi, compoundLensAddress);
-  // return await lens.methods.cTokenBalances(cUSDCAddress, owner).call({ from: owner });
+export async function printClaimableComp() {
+  const instance = contract<CompoundLensAbi>(require("../abi/CompoundLensAbi.json"), compoundLensAddress);
+  const result = await instance.methods
+    .getCompBalanceMetadataExt(compTokenAddress, unitrollerAddress, CONTRACT_ADDRESS)
+    .call({ from: OWNER });
+  const claimable = bn(result[0]).add(bn(result[3]));
+  console.log("claimable COMP", fmt(claimable));
+
+  // from CompoundLens:
+  //      function getCompBalanceMetadataExt(Comp comp, ComptrollerLensInterface comptroller, address account) external returns (CompBalanceMetadataExt memory) {
+  //         uint balance = comp.balanceOf(account);
+  //         comptroller.claimComp(account);
+  //         uint newBalance = comp.balanceOf(account);
+  //         uint accrued = comptroller.compAccrued(account);
+  //         uint total = add(accrued, newBalance, "sum comp total");
+  //         uint allocated = sub(total, balance, "sub allocated");
+  //
+  //         return CompBalanceMetadataExt({
+  //             balance: balance,
+  //             votes: uint256(comp.getCurrentVotes(account)),
+  //             delegate: comp.delegates(account),
+  //             allocated: allocated
+  //         });
+  //     }
+}
+
+export async function printCurrentLiquidity() {
+  const instance = contract<CompoundLoop>(abi, CONTRACT_ADDRESS);
+  const result = await instance.methods.getAccountLiquidityWithInterest().call({ from: OWNER });
+  console.log(await hre().web3.eth.getBlockNumber(), fmt(result.accountLiquidity));
+}
+
+export async function printHistoricalLiquidity(hours: number) {
+  const instance = contract<CompoundLoop>(abi, CONTRACT_ADDRESS);
+  const startBlock = 11792730;
+  const endBlock = await hre().web3.eth.getBlockNumber();
+
+  const promises = [];
+  for (let block = startBlock; block < endBlock; block += Math.round((60 * 60 * hours) / 13)) {
+    console.log("checking", block);
+    // @ts-ignore
+    promises.push({ block, p: instance.methods.getAccountLiquidityWithInterest().call({ from: OWNER }, block) });
+  }
+  const results = await Promise.all(promises.map((promise: any) => resolve(promise)));
+  results.forEach((r) => {
+    console.log(r.block, ":", r.p[1]);
+  });
+}
+
+async function resolve(promise: { block: number; p: any }): Promise<{ block: number; p: any }> {
+  return { block: promise.block, p: await keepTrying(() => promise.p) };
 }
--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,10 @@ export const ethAlchemyRpcUrl = "https://eth-mainnet.alchemyapi.io/v2/" + config
 export const etherscanApiBaseUrl = "https://api.etherscan.io/api";
 export const etherscanKey = config().etherscanKey;
 
-export const coinmarketkapKey = config().coinmarketcapKey;
+export const coinmarketcapKey = config().coinmarketcapKey;
+
+export const CONTRACT_ADDRESS = "0x53eBB911bA878F43F560fF4E4F08598694C26fa4";
+export const OWNER = "0x2a2f17DB6F5A9dFf54CA0594046635978c4bD396";
 
 export const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const unitrollerAddress = "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B";
